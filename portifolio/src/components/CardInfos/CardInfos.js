5ab38869d7091df7df32387d17b673619e967f88
João Hugo Leda <joaohugoleda2@gmail.com>
2022-01-09 22:53:24 -0300

    09-01

--- portifolio/src/components/CardInfos/CardInfos.js
+++ portifolio/src/components/CardInfos/CardInfos.js
@@ -2,32 +2,54 @@ import '../../style/card.scss'
 import './CardInfos.scss'
 
 import mapPin from '../../assets/map-pin.svg'
+import folder from '../../assets/folder.svg'
+import github from '../../assets/github.svg'
+import linkedin from '../../assets/linkedin.svg'
+import twitter from '../../assets/twitter.svg'
+import globe from '../../assets/globe.svg'
+import mail from '../../assets/mail.svg'
 
 export function CardInfos() {
   return(
-    <div className='card'>
+    <div className='card infos'>
       <div className='linha'>
-        <i href={mapPin} />
-        <span>Brasil</span>
+        <img src={mapPin} />
+        <span>
+          Brasil
+        </span>
       </div>
       <div className='linha'>
-        
+      <img src={folder} />
+        <span>
+          Estudante
+        </span>
       </div>
       <div className='linha'>
-        
+      <img src={github} />
+        <span>
+          HugoLeda
+        </span>
       </div>
       <div className='linha'>
-        
+      <img src={linkedin} />
+        <span>
+          Joao Hugo Leda
+        </span>
       </div>
       <div className='linha'>
-        
+      <img src={globe} />
+        <span>
+          https://github.com/hugoleda
+        </span>
       </div>
       <div className='linha'>
-        
-      </div>
-      <div className='linha'>
-        
-      </div>
+      
+        <img src={mail}></img>
+      
+        <span>
+          joaohugok4@gmail.com
+        </span>
+      </div>      
     </div>
   )
 }
