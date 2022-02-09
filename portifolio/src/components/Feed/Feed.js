f15dae64677e2bc7e1a57318d7cf0cbf23b34bf4
João Hugo Leda <joaohugoleda2@gmail.com>
2022-02-08 22:41:20 -0300

    08-02

--- portifolio/src/components/Feed/Feed.js
+++ portifolio/src/components/Feed/Feed.js
@@ -1,6 +1,11 @@
 import './Feed.scss'
 import '../../style/card.scss'
 
+import folder from'../../assets/folder.svg'
+import star from '../../assets/star.svg'
+import git_branch from '../../assets/git-branch.svg'
+import briefcase from '../../assets/briefcase.svg'
+
 export function Feed() {
   return (
     <div className='feed'>
@@ -9,8 +14,31 @@ export function Feed() {
           <h3>Meus projetos</h3>
           <a href='https://github.com/hugoleda?tab=repositories' target='blank'>Veja todos</a>
         </div>
-        <div className='card card-projeto'>
-          
+        <div className="cards-projeto">
+          <div className='card card-projeto'>
+            <div className='top'>
+              <img src={folder} alt='icone pasta'/>
+              <span>salao-de-belesa</span>
+            </div>
+            <p>
+              Software desenvolvido para o gerenciamento do Salão da Laura: @salao_dalaura
+            </p>
+            <div className='botton'>
+              <div>
+                <img src={star} alt='estrela' />
+                <span>1</span>
+                <img src={git_branch} alt='branch' />
+                <span>5</span>
+              </div>
+              <div>
+                <div className='linguagem'></div>
+                <span>Pascal</span>
+              </div>
+            </div>
+          </div>
+          <div className='card card-projeto'>
+            
+          </div>
         </div>
       </div>
     </div>
